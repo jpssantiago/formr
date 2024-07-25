a51ce000c84531e4270838f9c4ca37031990bed6
jpssantiago <joao@joaosantiago.com.br>
2024-07-25 14:43:34 -0300

    fix: title and description textareas were not changing the height to adjust to the text

--- src/components/form-page/create/create-question-form.tsx
+++ src/components/form-page/create/create-question-form.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { createRef, useEffect } from "react"
 import { ArrowRight } from "lucide-react"
 
 import { TQuestion } from "@/models/question"
@@ -16,6 +17,21 @@ type CreateQuestionFormProps = {
 export function CreateQuestionForm({ question }: CreateQuestionFormProps) {
     const { selectedQuestion, updateQuestion } = useCreateForm()
 
+    const titleInputRef = createRef<HTMLTextAreaElement>()
+    const descriptionInputRef = createRef<HTMLTextAreaElement>()
+
+    useEffect(() => {
+        if (titleInputRef.current) {
+            titleInputRef.current.style.height = "28px"
+            titleInputRef.current.style.height = `${titleInputRef.current.scrollHeight}px`
+        }
+
+        if (descriptionInputRef.current) {
+            descriptionInputRef.current.style.height = "24px"
+            descriptionInputRef.current.style.height = `${descriptionInputRef.current.scrollHeight}px`
+        }
+    }, [selectedQuestion])
+
     return (
         <div className="flex items-start gap-3 w-full max-w-[500px]">
             <div className="flex items-center gap-1 mt-0.5 text-[#0545AF]">
@@ -31,6 +47,7 @@ export function CreateQuestionForm({ question }: CreateQuestionFormProps) {
             <div className="flex flex-col gap-4 w-full">
                 <div className="space-y-2">
                     <Textarea
+                        ref={titleInputRef}
                         placeholder="Your question here..."
                         className="p-0 border-none focus-visible:ring-0 focus-visible:ring-offset-0 w-full h-7 min-h-7 font-medium text-xl resize-none"
                         value={selectedQuestion?.title}
@@ -48,6 +65,7 @@ export function CreateQuestionForm({ question }: CreateQuestionFormProps) {
                     />
 
                     <Textarea
+                        ref={descriptionInputRef}
                         placeholder="Description (optional)"
                         className="p-0 border-none focus-visible:ring-0 focus-visible:ring-offset-0 w-full h-6 min-h-6 text-base text-zinc-500 placeholder:text-zinc-500 resize-none"
                         value={selectedQuestion?.description}
