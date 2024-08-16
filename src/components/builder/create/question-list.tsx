49d56ca7e71bb97a413d3f5d4ceae8e93613ec6e
João Pedro Santiago <joao@joaosantiago.com.br>
2024-08-16 12:00:03 -0300

    feat: reorder list (drag and drop) to organize the questions of the form. It also removes the order property from the question table

--- src/components/builder/create/question-list.tsx
+++ src/components/builder/create/question-list.tsx
@@ -1,21 +1,36 @@
 "use client"
 
+import { Reorder } from "framer-motion"
+
 import { useCreateForm } from "@/contexts/create-form-context"
 import { QuestionListItem } from "./question-list-item"
 
 export function QuestionList() {
-    const { questions, selectedQuestion, selectQuestion } = useCreateForm()
+    const { questions, selectedQuestion, selectQuestion, reorderQuestions } = useCreateForm()
 
     return (
         <div className="space-y-1 large:hidden bg-zinc-100 bg-scroll custom-scroll p-3 rounded-lg w-60 h-full overflow-y-auto">
-            {questions.map((question) => (
-                <QuestionListItem
-                    key={question.id}
-                    question={question}
-                    onSelect={() => selectQuestion(question)}
-                    isSelected={selectedQuestion?.id == question.id}
-                />
-            ))}
+            <Reorder.Group
+                values={questions}
+                onReorder={reorderQuestions}
+                axis="y"
+            >
+                {questions.map((question) => (
+                    <Reorder.Item
+                        key={question.id}
+                        value={question}
+                        className="relative z-50 py-1"
+                        onDragStart={() => selectQuestion(question)}
+                    >
+                        <QuestionListItem
+                            key={question.id}
+                            question={question}
+                            onSelect={() => selectQuestion(question)}
+                            isSelected={selectedQuestion?.id == question.id}
+                        />
+                    </Reorder.Item>
+                ))}
+            </Reorder.Group>
         </div>
     )
 }
